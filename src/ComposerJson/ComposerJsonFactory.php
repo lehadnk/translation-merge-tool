b06022c27cb44075dea16033e9d2d1995d20754d
Alexey Zauzin <lehadnk@gmail.com>
2018-10-13 05:09:52 +0300

    It's now possible to get package version

---
+++ src/ComposerJson/ComposerJsonFactory.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lehadnk
+ * Date: 10/13/18
+ * Time: 5:06 AM
+ */
+
+namespace TranslationMergeTool\ComposerJson;
+
+
+class ComposerJsonFactory
+{
+
+}
