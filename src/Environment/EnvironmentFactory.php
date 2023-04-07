9aada68ffb80b1f595fd7872b56cca3210ae6397
Алексей Заузин <lehadnk@gmail.com>
2023-04-07 14:48:06 +0300

    Fixed the bug causing empty env vars causing the application to fail

--- src/Environment/EnvironmentFactory.php
+++ src/Environment/EnvironmentFactory.php
@@ -7,11 +7,21 @@ class EnvironmentFactory
     public function build(): Environment
     {
         return new Environment(
-            getenv('I18N_MRG_GITLAB_AUTH_TOKEN'),
-            getenv('I18N_MRG_GITHUB_AUTH_TOKEN'),
-            getenv('I18N_MRG_BITBUCKET_USERNAME'),
-            getenv('I18N_MRG_BITBUCKET_PASSWORD'),
-            getenv('I18N_WEBLATE_AUTH_TOKEN'),
+            $this->getEnvironmentVariable('I18N_MRG_GITLAB_AUTH_TOKEN'),
+            $this->getEnvironmentVariable('I18N_MRG_GITHUB_AUTH_TOKEN'),
+            $this->getEnvironmentVariable('I18N_MRG_BITBUCKET_USERNAME'),
+            $this->getEnvironmentVariable('I18N_MRG_BITBUCKET_PASSWORD'),
+            $this->getEnvironmentVariable('I18N_WEBLATE_AUTH_TOKEN'),
         );
+    }
+
+    private function getEnvironmentVariable(string $name): ?string
+    {
+        $value = getenv($name);
+        if ($value === false) {
+            return null;
+        }
+
+        return $value;
     }
 }
